5cdb16c45b9ec23dfeece21fdd0cd37273452716
Campbell McChesney <30006500@g.toiohomai.ac.nz>
2019-03-15 13:05:00 +1300

    Ex Complete

---
+++ ListObjectEx1/ListObjectEx1/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ListObjectEx1
+{
+    public class Person
+    {
+        
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string gender { get; set; }
+        public string region { get; set; }
+        public string title { get; set; }
+        public string phone { get; set; }
+        public Birthday birthday { get; set; }
+        public CreditCard credit_card { get; set; }       
+        public string email { get; set; }
+        public string password { get; set; }        
+        public string photo { get; set; }
+
+    }
+
+    public class Birthday
+    {
+        public string dmy { get; set; }
+        public string mdy { get; set; }
+        public string raw { get; set; }
+    }
+
+    public class CreditCard
+    {
+        public string expiration { get; set; }
+        public string number { get; set; }
+        public string pin { get; set; }
+        public string security { get; set; }
+
+    }
+}
---
+++ ListObjectEx1/ListObjectEx1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+using System.Web.Script.Serialization;
+
+
+namespace ListObjectEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = new WebClient().DownloadString("https://uinames.com/api/?ext&amount=10");
+            List<Person> data = new JavaScriptSerializer().Deserialize<List<Person>>(json);
+            foreach (Person x in data)
+            {
+                Console.WriteLine("Title: {0}\nName: {1} {2}\nGender: {3}\nRegion: {4}\nPhone: {5}\nBirthdate\nDMY: {6}\nMDY: {7}\nRAW: {8}\nCredit Card Details\nCard Number: {9}\nExpiration {10}\nPIN: {11}\nCVC: {12}\nPhotoURL: {13}\n", x.title, x.name, x.surname, x.gender, x.region, x.phone, x.birthday.dmy, x.birthday.mdy, x.birthday.raw, x.credit_card.number, x.credit_card.expiration, x.credit_card.pin, x.credit_card.security, x.photo);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
